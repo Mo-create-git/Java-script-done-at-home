55c9be9610efb71d61141cad3fa4bf84538c3d15
Mo-create-git <msaffan078@gmail.com>
2025-06-03 00:03:13 +0530

    while loop concept

---
+++ video-08.js
@@ -0,0 +1,14 @@
+//while loop.
+//while loop is used to repeat a block of code as long as a specified condition is true.
+
+//the loop checks the condition before running the block
+//if the condition is false at the beginnning , the  loop body will not run 
+
+//ex:
+let i =1;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+
